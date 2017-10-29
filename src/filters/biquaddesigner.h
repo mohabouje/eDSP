f541b238f7e47e8b60b8c2f13e0d50a851c23227
Mohammed Boujemaoui <mohabouje@gmail.com>
2017-10-29 12:33:28 +0100

    Finishing the different equations by Robert Bristow-Johnson

--- src/filters/biquaddesigner.h
+++ src/filters/biquaddesigner.h
@@ -5,6 +5,7 @@
 #ifndef EDSP_BIQUADDESIGNER_H
 #define EDSP_BIQUADDESIGNER_H
 
+#include <tclDecls.h>
 #include "base/algoritmtemplate.h"
 #include "base/constants.h"
 #include "biquad.h"
@@ -86,6 +87,91 @@ EDSP_BEGING_NAMESPACE
                 b[2] = static_cast<T>((1 - K / Q + K2) * norm);
                 return Biquad<T>(a,b);
             }
+
+
+            template <typename T>
+            Biquad<T> peak(T frequency, T Q, T gain) {
+                auto K = static_cast<T>(std::tan(Constants<T>::pi * frequency));
+                auto K2 = static_cast<T>(K * K);
+                auto V = static_cast<T>(std::exp(std::abs(gain) * static_cast<T>(0.05f) * std::log10(200)));
+
+                BiquadCoefficients a, b;
+                if (gain >= 0) {
+                    auto norm = static_cast<T>(1 / (1 + 1 / Q * K + K2));
+                    a[0] = static_cast<T>((1 + V / Q * K + K2) * norm);
+                    a[1] = static_cast<T>(2 * (K2 - 1) * norm);
+                    a[2] = static_cast<T>((1 - V / Q * K + K2) * norm);
+                    b[0] = static_cast<T>(1);
+                    b[1] = static_cast<T>(a[1]);
+                    b[2] = static_cast<T>((1 - 1 / Q * K + K2) * norm);
+                }
+                else {
+                    auto norm = static_cast<T>(1 / (1 + V / Q * K + K2));
+                    a[0] = static_cast<T>((1 + 1 / Q * K + K2) * norm);
+                    a[1] = static_cast<T>(2 * (K2 - 1) * norm);
+                    a[2] = static_cast<T>((1 - 1 / Q * K + K2) * norm);
+                    b[0] = static_cast<T>(1);
+                    b[1] = static_cast<T>(a[1]);
+                    b[2] = static_cast<T>((1 - V / Q * K + K2) * norm);
+                }
+                return Biquad<T>(a,b);
+            }
+
+            template <typename T>
+            Biquad<T> lowshelf(T frequency, T gain) {
+                auto K = static_cast<T>(std::tan(Constants<T>::pi * frequency));
+                auto K2 = static_cast<T>(K * K);
+                auto V  = static_cast<T>(std::exp(std::fabs(gain) * static_cast<T>(0.05f) * std::log10(200)));
+
+                BiquadCoefficients a, b;
+                if (gain >= 0) {
+                    auto norm = 1 / (1 + std::sqrt(2) * K + K2);
+                    a[0] = static_cast<T>((1 + std::sqrt(2 * V) * K + V * K2) * norm);
+                    a[1] = static_cast<T>(2 * (V * K2 - 1) * norm);
+                    a[2] = static_cast<T>((1 - std::sqrt(2 * V) * K + V * K2) * norm);
+                    b[0] = static_cast<T>(1);
+                    b[1] = static_cast<T>(2 * (K2 - 1) * norm);
+                    b[2] = static_cast<T>((1 - std::sqrt(2) * K + K2) * norm);
+                } else {
+                    auto norm = static_cast<T>(1 / (1 + std::sqrt(2 * V) * K + V * K2));
+                    a[0] = static_cast<T>((1 + std::sqrt(2) * K + K2) * norm);
+                    a[1] = static_cast<T>(2 * (K2 - 1) * norm);
+                    a[2] = static_cast<T>((1 - std::sqrt(2) * K + K2) * norm);
+                    b[0] = static_cast<T>(1);
+                    b[1] = static_cast<T>(2 * (V * K2 - 1) * norm);
+                    b[2] = static_cast<T>((1 - std::sqrt(2 * V) * K + V * K2) * norm);
+                }
+                return Biquad<T>(a, b);
+            }
+
+            template<typename T>
+            Biquad<T> highshelf(T frequency, T gain) {
+                auto K = static_cast<T>(std::tan(Constants<T>::pi * frequency));
+                auto K2 = static_cast<T>(K * K);
+                auto V  = static_cast<T>(std::exp(std::fabs(gain) * static_cast<T>(0.05f) * std::log10(200)));
+
+                BiquadCoefficients a, b;
+                if (gain >= 0) {
+                    auto norm =static_cast<T>( 1 / (1 + std::sqrt(2) * K + K2));
+                    a[0] = static_cast<T>((V + std::sqrt(2 * V) * K + K2) * norm);
+                    a[1] = static_cast<T>(2 * (K2 - V) * norm);
+                    a[2] = static_cast<T>((V - std::sqrt(2 * V) * K + K2) * norm);
+                    b[0] = static_cast<T>(1);
+                    b[1] = static_cast<T>(2 * (K2 - 1) * norm);
+                    b[2] = static_cast<T>((1 - std::sqrt(2) * K + K2) * norm);
+                } else {
+                    auto norm = static_cast<T>(1 / (V + std::sqrt(2 * V) * K + K2));
+                    a[0]   = static_cast<T>((1 + std::sqrt(2) * K + K2) * norm);
+                    a[1]   = static_cast<T>(2 * (K2 - 1) * norm);
+                    a[2]   = static_cast<T>((1 - std::sqrt(2) * K + K2) * norm);
+                    b[0] = static_cast<T>(1);
+                    b[1]   = static_cast<T>(2 * (K2 - V) * norm);
+                    b[2]   = static_cast<T>((V - std::sqrt(2 * V) * K + K2) * norm);
+                }
+                return Biquad<T>(a, b);
+            }
+
+
         }
     }
 EDSP_END_NAMESPACE
