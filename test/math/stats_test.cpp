38652f4b6e9127afc0b0f4f5ae22f30a47453dad
Mohammed Boujemaoui <mohabouje@gmail.com>
2017-12-08 23:42:11 +0100

    Find some problems in the stats functions.

--- test/math/stats_test.cpp
+++ test/math/stats_test.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "../test_db.h"
-#include "../test_util.h"
+#include "../test_config.h"
 #include "math/stats.h"
 #include <catch.hpp>
 
@@ -16,25 +16,29 @@ SCENARIO("Testing the basic usage of the statistic functions") {
         // Testing mean function
         WHEN("We want to estimate the mean of the signal") {
             const double mean = stats::mean(std::begin(test), std::end(test));
-            //REQUIRE(test::util::isEqual(mean), test::db::mean_ref_vector);
+            const Approx target = Approx(mean).epsilon(TEST_TOLERANCE);
+            REQUIRE(target == test::db::mean_ref_vector);
         }
 
         // Testing mode function
         WHEN("We want to estimate the mode of the signal") {
             const double mode = stats::mode(test);
-            //REQUIRE(mode == test::db::mode_ref_vector);
+            const Approx target = Approx(mode).epsilon(TEST_TOLERANCE);
+            REQUIRE(target == test::db::mode_ref_vector);
         }
 
         // Testing variance function
         WHEN("We want to estimate the variance of the signal") {
             const double var = stats::variance(std::begin(test), std::end(test));
-            //REQUIRE(var == test::db::var_ref_vector);
+            const Approx target = Approx(var).epsilon(TEST_TOLERANCE);
+            REQUIRE(target == test::db::var_ref_vector);
         }
 
         // Testing standard desviation function
         WHEN("We want to estimate the standard desviation of the signal") {
-            const double std = stats::standar_desviation(std::begin(test), std::end(test));
-            //REQUIRE(std == test::db::std_desv_ref_vector);
+            const double _std = stats::standar_desviation(std::begin(test), std::end(test));
+            const Approx target = Approx(_std).epsilon(TEST_TOLERANCE);
+            REQUIRE(target == test::db::std_desv_ref_vector);
         }
 
         // Testing mean error
