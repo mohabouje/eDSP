0485b9c292e0693138dbebcd7da23a1b382a3fdb
Mohammed Boujemaoui <mohabouje@gmail.com>
2017-12-09 16:03:33 +0100

    Fixing conditions inside the buffer that computes the basic transforms.

--- include/frequency/dct.h
+++ include/frequency/dct.h
@@ -21,7 +21,7 @@ EDSP_BEGING_NAMESPACE
             template <class InputIterator, class OutputIterator>
             void compute_r2r(InputIterator __first, InputIterator __last, OutputIterator __out) {
                 const auto m_size = std::distance(__first, __last);
-                if (size != m_size && plan != nullptr) {
+                if (size != m_size || plan == nullptr) {
                     fftw_destroy_plan(plan);
                     size = m_size;
                     plan = fftw_plan_r2r_1d(static_cast<int>(size),
