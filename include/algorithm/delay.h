4551605b029c097dd2b8cee912b2f486c3ed3e27
Mohammed Boujemaoui <mohabouje@gmail.com>
2017-12-10 13:09:23 +0100

    First algorithm: delay implementation.

---
+++ include/algorithm/delay.h
@@ -0,0 +1,80 @@
+//
+// Created by Mohammed Boujemaoui on 10/12/2017.
+//
+
+#ifndef EDSP_DELAY_H
+#define EDSP_DELAY_H
+
+#include "config.h"
+#include "utility/vector.h"
+#include <vector>
+#include <assert.h>
+
+EDSP_BEGIN_NAMESPACE
+    namespace algorithm {
+
+        template <typename T>
+        class Delay {
+        public:
+            Delay(size_t delay, size_t max_delay) :
+                    m_delay(delay),
+                    m_max_delay(max_delay),
+                    m_buffer{max_delay, 0}
+            {
+            }
+            virtual ~Delay() = default;
+
+
+            template <class InputIterator, class OutputIterator>
+            void compute(InputIterator first, InputIterator last, OutputIterator out) {
+                for (; first != last; ++first, ++out) {
+                    m_buffer[input_point++] = *first * m_gain;
+                    *out = m_buffer[output_point++];
+                    if (input_point == m_max_delay) input_point = 0;
+                    if (output_point == m_max_delay) output_point = 0;
+                };
+            }
+
+            void compute(const T in, T& out) {
+                m_buffer[input_point++] = in * m_gain;
+                out = m_buffer[output_point++];
+                if (input_point == m_max_delay) input_point = 0;
+                if (output_point == m_max_delay) output_point = 0;
+            }
+
+            std::size_t delay() const { return m_delay; }
+            std::size_t max_delay() const { return m_max_delay; }
+            T gain() const { return m_gain; }
+
+            void set_gain(const T value) { m_gain = value; }
+            void set_delay(const std::size_t value) {
+                assert(value <= m_max_delay && "Greater than the maximum");
+                m_delay = value;
+            }
+            void set_max_delay(const std::size_t value) {
+                if (m_max_delay != value) {
+                    m_max_delay = value;
+                    m_buffer.resize(value);
+                    reset();
+                }
+            }
+
+
+            void reset() {
+                utility::set(std::begin(m_buffer), std::end(m_buffer), 0);
+                input_point = 0;
+                output_point = 0;
+            }
+
+        private:
+            T           m_gain;
+            std::size_t m_delay{0};
+            std::size_t m_max_delay{1};
+            std::vector<T> m_buffer;
+            std::size_t input_point{0};
+            std::size_t output_point{0};
+        };
+    }
+EDSP_END_NAMESPACE
+
+#endif //EDSP_DELAY_H
