f0acca819b8993c1885bfe01ec446701cc2237d5
Mohammed Boujemaoui <mohabouje@gmail.com>
2018-03-23 03:24:43 +0100

    Implementing Bartlett-Hanning window.

---
+++ libedsp/include/window/barthann.h
@@ -0,0 +1,47 @@
+/*
+ * eDSP, A cross-platform DSP framework written in C++.
+ * Copyright (C) 2018 Mohammed Boujemaoui Boulaghmoudi
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef EDSP_WINDOW_BATHANNWIN_H
+#define EDSP_WINDOW_BATHANNWIN_H
+
+#include "window.h"
+
+EDSP_BEGIN_NAMESPACE
+    /**
+     * @brief Bartlett-Hann %window implementation.
+     *
+     * See also: Bartlett and Hann.
+     *
+     */
+    class BartHann : Window {
+    public:
+
+        /**
+         * @brief Creates and computes a Blackman %window with the given size.
+         * @param size The number of elements to initially create.
+         */
+        explicit BartHann(size_type size);
+        ~BartHann() override;
+
+        void compute() override;
+    };
+EDSP_END_NAMESPACE
+
+
+
+#endif //EDSP_WINDOW_BATHANNWIN_H
