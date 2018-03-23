d6b09971b5ee546d85d84a28907fedb904f1fbdd
Mohammed Boujemaoui <mohabouje@gmail.com>
2018-03-23 03:41:30 +0100

    Implementing rectangular, Hamming & Hanning

--- libedsp/include/window/hamming.h
+++ libedsp/include/window/hamming.h
@@ -1,6 +1,20 @@
-//
-// Created by mohabouje on 21/03/18.
-//
+/*
+ * eDSP, A cross-platform DSP framework written in C++.
+ * Copyright (C) 2018 Mohammed Boujemaoui Boulaghmoudi
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef EDSP_WINDOW_HAMMING_H
 #define EDSP_WINDOW_HAMMING_H
@@ -24,10 +38,18 @@ EDSP_BEGIN_NAMESPACE
     public:
 
         /**
+         * @brief Creates and computes a Hamming %window with the given size & type
+         * @param size The number of elements to initially create.
+         * @param type Window type.
+         */
+        Hamming(size_type size, WindowType type);
+
+        /**
          * @brief Creates and computes a Hamming %window with the given size.
          * @param size The number of elements to initially create.
          */
         explicit Hamming(size_type size);
+
         ~Hamming() override;
 
         void compute() override;
--- libedsp/src/window/hanning.cpp
+++ libedsp/src/window/hanning.cpp
@@ -1,6 +1,20 @@
-//
-// Created by mohabouje on 21/03/18.
-//
+/*
+ * eDSP, A cross-platform DSP framework written in C++.
+ * Copyright (C) 2018 Mohammed Boujemaoui Boulaghmoudi
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "window/hanning.h"
 #include "base/constants.h"
@@ -10,12 +24,17 @@ Hanning::Hanning(edsp::Window::size_type size) : Window(size) {
 
 }
 
-void Hanning::compute() {
-    for (size_type sz = size(), i = 0; i < sz; ++i) {
-        data_[i] = 0.5 - 0.5 * std::cos(2 * Constants<double>::pi * i / (sz - 1.0));
-    }
+Hanning::Hanning(Window::size_type size, Window::WindowType type) : Window(size, type) {
+
 }
 
-Hanning::~Hanning() {
+Hanning::~Hanning() = default;
 
+void Hanning::compute() {
+    if (!empty()) {
+        const value_type N = (type_ == WindowType::Symmetric) ? size() - 1 : size();
+        for (size_type i = 0, sz = size(); i < sz; ++i) {
+            data_[i] = 0.5 * (1 - std::cos(2 * Constants<value_type>::pi * i / (N)));
+        }
+    }
 }
