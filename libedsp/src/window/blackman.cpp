60e383f2ab95ceefd93ba5cbd8b522791a84b332
Mohammed Boujemaoui <mohabouje@gmail.com>
2018-03-23 03:34:50 +0100

    Implementing Blackman window.

--- libedsp/src/window/blackman.cpp
+++ libedsp/src/window/blackman.cpp
@@ -1,7 +1,20 @@
-//
-// Created by mohabouje on 21/03/18.
-//
-
+/*
+ * eDSP, A cross-platform DSP framework written in C++.
+ * Copyright (C) 2018 Mohammed Boujemaoui Boulaghmoudi
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "window/blackman.h"
 #include "base/constants.h"
@@ -11,13 +24,22 @@ Blackman::Blackman(edsp::Window::size_type size) : Window(size) {
 
 }
 
+Blackman::Blackman(Window::size_type size, Window::WindowType type) : Window(size, type) {
+
+}
+
+
 void Blackman::compute() {
-    for (size_type sz = size(), i = 0; i < sz; ++i) {
-        data_[i] = 0.42 - 0.5 * std::cos(2 * Constants<double>::pi * i / (sz - 1.0f))
-                   + 0.08 * std::cos((4 * Constants<double>::pi * i) / (sz - 1.0f));
+    if (!empty()) {
+        const value_type N = (type_ == WindowType::Symmetric) ? size() - 1 : size();
+        for (size_type i = 0, sz = size(); i < sz; ++i) {
+            data_[i] = 0.42 - 0.5 * std::cos(2. * Constants<value_type>::pi * i / N)
+                       + 0.08 * std::cos (4 * Constants<value_type>::pi * i / N);
+        }
     }
 }
 
 Blackman::~Blackman() {
 
 }
+
