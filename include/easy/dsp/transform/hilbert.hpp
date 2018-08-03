f98c09b741c7fe705ff6e71d365434efff882eb8
Mohammed Boujemaoui Boulaghmoudi <mohabouje@gmail.com>
2018-08-03 12:19:53 +0200

    Removing the subsection: smoothing. Moving it to the filter subfolder.

---
+++ include/easy/dsp/transform/hilbert.hpp
@@ -0,0 +1,45 @@
+/*
+ * EasyDSP, A cross-platform Digital Signal Processing library written in modern C++.
+ * Copyright (C) 2018 Mohammed Boujemaoui Boulaghmoudi
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along withº
+ * this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * Filename: hilbert.hpp
+ * Created at: 09/06/18
+ * Created by: Mohammed Boujemaoui
+ */
+
+#ifndef EASYDSP_HILBERT_HPP
+#define EASYDSP_HILBERT_HPP
+
+#include "fft_impl.hpp"
+
+namespace easy { namespace dsp {
+
+    template <typename InputIterator, typename OutputIterator>
+    inline void hilbert(InputIterator first, InputIterator last, OutputIterator out) {
+        using value_type = typename std::iterator_traits<InputIterator>::value_type;
+        fftw_plan<value_type> plan;
+        plan.dht(fftw_cast(&(*first)), fftw_cast(&(*out)),
+                 static_cast<typename fftw_plan<value_type>::size_type>(std::distance(first, last)));
+    }
+
+    template <typename Container>
+    inline void dht(const Container& input, Container& output) {
+        hilbert(std::cbegin(input), std::cend(input), std::begin(output));
+    }
+
+}} // namespace easy::dsp
+
+#endif // EASYDSP_HILBERT_HPP
