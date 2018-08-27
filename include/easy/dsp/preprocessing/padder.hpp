a2783903986ea89eb8bd1f31819ece49ef6c5732
Mohammed Boujemaoui Boulaghmoudi <mohabouje@gmail.com>
2018-08-27 12:21:16 +0200

    Update padder.hpp

--- include/easy/dsp/preprocessing/padder.hpp
+++ include/easy/dsp/preprocessing/padder.hpp
@@ -38,10 +38,11 @@ namespace easy { namespace dsp {
         const auto output_size = std::distance(first_out, last_out);
         meta::expects(output_size >= input_size, "Output size should be greather or equal than the input size");
         std::copy(first, last, first_out);
-        std::generate(meta::advance(first_out, input_size), last, generator);
+        std::generate(meta::advance(first_out, input_size), last_out, generator);
     };
 
-    template <typename InputIterator, typename OutputIterator>
+    template <typename InputIterator, 
+              typename OutputIterator>
     constexpr void padder(InputIterator first, InputIterator last, OutputIterator first_out, OutputIterator last_out,
                           typename std::iterator_traits<InputIterator>::value_type value) {
         padder(first, last, first_out, last_out,
