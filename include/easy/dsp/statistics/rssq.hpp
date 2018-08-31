f69d2fd2680f7a6fa834b8699c5c6bf4f4f9d1cc
Mohammed Boujemaoui <mohabouje@gmail.com>
2018-09-01 01:19:00 +0200

    Adding two new utilities files to perform the RSSQ calculation and a peak2peak computation.

---
+++ include/easy/dsp/statistics/rssq.hpp
@@ -0,0 +1,41 @@
+/*
+ * EasyDSP, A cross-platform Digital Signal Processing library written in modern C++.
+ * Copyright (C) 2018 Mohammed Boujemaoui Boulaghmoudi
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along withº
+ * this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * Filename: rms.hpp
+ * Author: Mohammed Boujemaoui
+ * Date: 01/08/2018
+ */
+#ifndef EASYDSP_RSSQ_HPP
+#define EASYDSP_RSSQ_HPP
+
+#include <easy/dsp/statistics/rms.hpp>
+#include <numeric>
+#include <cmath>
+
+namespace easy { namespace dsp {
+
+    template <typename InputIterator,
+              typename value_type = typename std::iterator_traits<InputIterator>::value_type>
+    constexpr value_type rssq(InputIterator first, InputIterator last) {
+        const value_type sum_square = std::inner_product(first, last, first, static_cast<value_type>(0));
+        return std::sqrt(sum_square);
+    }
+
+}} // namespace easy::dsp
+
+#endif // EASYDSP_RSSQ_HPP
+
---
+++ include/easy/dsp/utilities/peak2peak.hpp
@@ -0,0 +1,39 @@
+/*
+ * EasyDSP, A cross-platform Digital Signal Processing library written in modern C++.
+ * Copyright (C) 2018 Mohammed Boujemaoui Boulaghmoudi
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along withº
+ * this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * Filename: peak2peak.hpp
+ * Author: Mohammed Boujemaoui
+ * Date: 2/8/2018
+ */
+#ifndef EASYDSP_PEAK2PEAK_HPP
+#define EASYDSP_PEAK2PEAK_HPP
+
+#include <easy/meta/expects.hpp>
+#include <cmath>
+#include <algorithm>
+
+namespace easy { namespace dsp {
+
+    template <typename InputIterator,
+              typename value_type = typename std::iterator_traits<InputIterator>::value_type>
+    constexpr value_type peak2peak(InputIterator first, InputIterator last) noexcept {
+        const auto pair = std::minmax_element(first, last);
+        return std::abs(pair.second) - std::abs(pair.first);
+    }
+}}
+
+#endif // EASYDSP_RMS2PEAK_HPP
