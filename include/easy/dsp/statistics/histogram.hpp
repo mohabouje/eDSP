870fb2a00f4a89ce0abf56e56a567115878056c3
Mohammed Boujemaoui Boulaghmoudi <mohabouje@gmail.com>
2018-10-02 17:13:09 +0200

    No more dependencies with Boost.

--- include/easy/dsp/statistics/histogram.hpp
+++ include/easy/dsp/statistics/histogram.hpp
@@ -22,9 +22,6 @@
 #ifndef EASYDSP_FISHER_HISTOGRAM_HPP
 #define EASYDSP_FISHER_HISTOGRAM_HPP
 
-#include <boost/accumulators/accumulators.hpp>
-#include <boost/accumulators/statistics/density.hpp>
-#include <boost/accumulators/statistics/stats.hpp>
 #include <vector>
 
 namespace easy { namespace dsp { namespace statistics {
@@ -62,18 +59,10 @@ namespace easy { namespace dsp { namespace statistics {
          */
         template <typename InIterator, typename OutIterator>
         constexpr void compute(InIterator first, InIterator last, OutIterator out) {
-            acc accumulator_(boost::accumulators::tag::density::num_bins   = num_bins_,
-                             boost::accumulators::tag::density::cache_size = cache_size_);
-            std::for_each(first, last, std::bind<void>(std::ref(accumulator_), std::placeholders::_1));
-            const histogram_type histogram = boost::accumulators::density(accumulator_);
-            std::copy(std::cbegin(histogram), std::cend(histogram), out);
+
         }
 
     private:
-        using density        = boost::accumulators::tag::density;
-        using feature        = boost::accumulators::features<density>;
-        using acc            = boost::accumulators::accumulator_set<T, feature>;
-        using histogram_type = boost::iterator_range<typename std::vector<std::pair<T, T>, Allocator>::iterator>;
         size_type num_bins_;
         size_type cache_size_;
     };
