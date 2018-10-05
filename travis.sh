e9f9c56d064969c9869f6085985455cc700ecabf
Mohammed Boujemaoui Boulaghmoudi <mohabouje@gmail.com>
2018-10-05 14:52:09 +0200

    EasyDSP is a header-only library it can be interesting to get a code coverage of everything. Not only the files used in the tests.

--- travis.sh
+++ travis.sh
@@ -11,7 +11,11 @@ workingprocess() { echo -e "${BB}$1${NC}"; }
 allert () { echo -e "${RED}$1${NC}"; }
 
 
-showinfo "Building the library..."
+showinfo "Starting the process"
+cd ${TRAVIS_BUILD_DIR}
+lcov --capture --initial --directory . --output-file header-only.info --no-external
+
+showinfo "1 - Building the library."
 mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=ON -DBUILD_DOCS=OFF -DENABLE_DEBUG_INFORMATION=ON ..
@@ -21,11 +25,21 @@ if [ $? -ne 0 ]; then
     exit 3
 fi
 
-showinfo "Running the tests..."
+showinfo "2 - Test & Code Coverage"
+workingprocess "Running the test..."
 make test
 if [ $? -ne 0 ]; then
     error "Error: there are some tests that failed!"
     exit 4
 fi
+
+workingprocess "Generating the test-only code coverage..."
+cd ${TRAVIS_BUILD_DIR}
+lcov --capture --directory . --ouput-file test-only.info
+lcov --add-tracefile header-only.info --add-tracefile test-onle.info --output-file coverage.info
+lcov --remove coverage.info '/usr/*' --output-file coverage.info
+
+workingprocess "Displaying the final results..."
+lcov --list coverage.info
 
 showinfo "Success: All tests compile and pass."
