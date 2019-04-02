701a1081cedff99f9b32ed349f942dbff1493b27
Mohammed Boujemaoui <mohabouje@gmail.com>
2019-04-02 23:37:35 +0200

    Updating some typos in the documentation

--- include/edsp/converter/rad2deg.hpp
+++ include/edsp/converter/rad2deg.hpp
@@ -12,7 +12,7 @@
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
  * more details.
  *
- * You should have received a copy of the GNU General Public License along withº
+ * You should have received a copy of the GNU General Public License along width
  * this program.  If not, see <http://www.gnu.org/licenses/>
  *
  * File: rad2deg.hpp
